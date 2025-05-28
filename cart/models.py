6f892856fda8f6db6aca790cc69613c5d58f5da2
Danya <sinrandomg@gmail.com>
2025-05-28 22:56:03 +0300

    cart model, admin

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from products.models import Product
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"User {self.user.email}: {self.product.name} x{self.quantity}"
+
+    @property
+    def total_price(self):
+        return self.quantity * self.product.price
+
+    class Meta:
+        verbose_name_plural = "Carts"
+        verbose_name = "Cart"
+        ordering = ['-created_at']
