8bec0ce52fb5381ef3c9a212eba5566881f3aa08
Danya <sinrandomg@gmail.com>
2025-05-28 23:28:01 +0300

    cart: adding products

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import cart_list
+from .views import cart_list, cart_add
 
 
 app_name = 'cart'
 urlpatterns = [
     path('user/cart/', cart_list, name='cart_list'),
+    path('cart/add/<int:product_id>/', cart_add, name='cart_add'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import Cart
@@ -5,3 +6,14 @@ from .models import Cart
 def cart_list(request):
     cart = Cart.objects.filter(user=request.user)
     return render(request, "cart/cart.html", context={"cart": cart})
+
+def cart_add(request, product_id):
+    cart = Cart.objects.filter(user=request.user, product_id=product_id)
+
+    if not cart.exists():
+        obj = Cart.objects.create(user=request.user, product_id=product_id, quantity=1)
+    else:
+        obj = cart.first()
+        obj.quantity += 1
+        obj.save()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
