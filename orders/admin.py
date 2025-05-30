4c0ac1d9e8ce6aae971f93227287df9ecbb6c2b2
Danya <sinrandomg@gmail.com>
2025-05-30 19:21:02 +0300

    create Order model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'status')
+    fields = (
+        'id', 'created_at',
+        ('first_name', 'last_name'), 'address',
+        'basket_history', 'status', 'user',)
+    readonly_fields = ('id', 'created_at')
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+from cart.models import Cart
+
+class Order(models.Model):
+    CREATED = 0
+    PAID = 1
+    ON_WAY = 2
+    DELIVERED = 3
+    STATUSES = (
+            (CREATED, 'Створено'),
+            (PAID, 'Сплачено'),
+            (ON_WAY, 'У дорозі'),
+            (DELIVERED, 'Доставлений'),
+        )
+
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    address = models.CharField(max_length=256)
+    status = models.SmallIntegerField(default=CREATED, choices=STATUSES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    basket_history = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f'Order #{self.id}. {self.first_name} {self.last_name}'
+
+    def update_after_payment(self):
+        cart = Cart.objects.filter(user=self.user)
+
+        total_sum = sum(item.total_price for item in cart)
+
+        self.status = self.PAID
+        self.total_price = total_sum
+
+        self.basket_history = {
+            'purchased_items': [
+                {
+                    'product': item.product.name,
+                    'quantity': item.quantity,
+                    'total_price': float(item.total_price)
+                }
+                for item in cart
+            ],
+            'total_sum': float(total_sum)
+        }
+        cart.delete()
+        self.save()
+
+    class Meta:
+        verbose_name_plural = "Orders"
+        verbose_name = "Order"
