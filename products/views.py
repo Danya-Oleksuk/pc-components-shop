23a0ba21664f23c8ea0e0a743fd6c7632fc20ccc
Danya <sinrandomg@gmail.com>
2025-06-04 21:45:50 +0300

    add cashe to random_products on main page

--- products/views.py
+++ products/views.py
@@ -11,7 +11,10 @@ class MainPageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        random_products = Product.objects.order_by("?")[:3]
+        random_products = cache.get("random_products")
+        if not random_products:
+            random_products = Product.objects.order_by("?")[:3]
+            cache.set("random_products", random_products, timeout=30)
         context["random_products"] = random_products
 
         return context
