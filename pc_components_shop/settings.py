6be3f867a589c04879bfdb231b0ce99c3fffd899
Danya <sinrandomg@gmail.com>
2025-06-06 13:06:58 +0300

    refactor: moved all inline styles to separate CSS files

--- pc_components_shop/settings.py
+++ pc_components_shop/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2!=i!7&li8zd-)i86=$(ef*7a@p-1&l9+xy6v18-$!dn5ti8vi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "*",
