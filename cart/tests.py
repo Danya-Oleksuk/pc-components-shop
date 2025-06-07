f61ab848127e077de9c977e55c9d961949f3a168
Danya <sinrandomg@gmail.com>
2025-06-07 10:41:42 +0300

    Add cart urls tests

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class CartURLsTests(TestCase):
+
+    def test_cart_list_url(self):
+        url = reverse("cart:cart_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_cart_add_url(self):
+        url = reverse("cart:cart_add", args=[1])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_cart_remove_url(self):
+        url = reverse("cart:cart_remove", args=[1])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
