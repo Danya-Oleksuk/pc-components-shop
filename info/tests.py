c008b094251d22d66d48ace821167ad960dd90e2
Danya <sinrandomg@gmail.com>
2025-06-07 10:29:28 +0300

    Add tests for info app views

--- info/tests.py
+++ info/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class InfoViewsTestCase(TestCase):
+
+    def test_about_view(self):
+        response = self.client.get(reverse("info:about"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "info/about.html")
+
+    def test_contacts_view(self):
+        response = self.client.get(reverse("info:contacts"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "info/contacts.html")
+
+    def test_terms_view(self):
+        response = self.client.get(reverse("info:terms"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "info/terms.html")
