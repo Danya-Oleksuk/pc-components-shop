99da7789193d06e81d991a8b0baf25b2eade7e24
Danya <sinrandomg@gmail.com>
2025-06-10 21:45:23 +0300

    feat: add contact page with message form and model

---
+++ info/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import ContactMessage
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "message")
+    search_fields = ("name", "email")
+    list_filter = ("name", "email")
--- info/views.py
+++ info/views.py
@@ -1,12 +1,19 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import ContactMessage
+from .forms import ContactForm
 
 
 class AboutView(TemplateView):
     template_name = "info/about.html"
 
 
-class ContactsView(TemplateView):
-    template_name = "info/contacts.html"
+class ContactsView(CreateView):
+    model = ContactMessage
+    form_class = ContactForm
+    template_name = 'info/contacts.html'
+    success_url = reverse_lazy('info:contacts')
 
 
 class TermsView(TemplateView):
