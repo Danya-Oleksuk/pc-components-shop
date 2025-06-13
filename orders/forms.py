ade00048ece4feb985f5d7f5f935e1572b8f93a5
Danya <sinrandomg@gmail.com>
2025-06-13 23:48:55 +0300

    fix: show error messages below input fields

--- orders/forms.py
+++ orders/forms.py
@@ -7,34 +7,40 @@ class OrderForm(forms.ModelForm):
     first_name = forms.CharField(
         widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Ім’я"}),
         required=True,
+        error_messages={
+            'required': 'Будь ласка, введіть ваше ім’я.'
+        }
     )
     last_name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Прізвище"}
-        ),
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Прізвище"}),
         required=True,
+        error_messages={
+            'required': 'Будь ласка, введіть ваше прізвище.'
+        }
     )
     phone = forms.CharField(
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Телефон"}
-        ),
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Телефон"}),
         required=True,
+        error_messages={
+            'required': 'Будь ласка, введіть номер телефону.'
+        }
     )
+
     city = forms.CharField(
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "id": "city-input", "placeholder": "Місто"}
-        ),
+        widget=forms.TextInput(attrs={"class": "form-control", "id": "city-input", "placeholder": "Місто"}),
         required=True,
+        error_messages={
+            'required': 'Будь ласка, введіть назву міста.'
+        }
     )
+
     warehouse = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                "class": "form-control",
-                "id": "warehouse-input",
-                "placeholder": "Відділення/Поштомат",
-            }
-        ),
+            attrs={"class": "form-control", "id": "warehouse-input", "placeholder": "Відділення/Поштомат"}),
         required=True,
+        error_messages={
+            'required': 'Будь ласка, вкажіть відділення або поштомат.'
+        }
     )
 
     def clean_phone(self):
