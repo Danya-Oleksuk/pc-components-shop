51d4c8cc0c5d6cc6a78fc1c3502b1ee254c9bf57
Danya <sinrandomg@gmail.com>
2025-06-28 19:42:02 +0300

    Add translations for checkout, order history pages

--- orders/forms.py
+++ orders/forms.py
@@ -1,57 +1,61 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from .models import Order
 
 
 class OrderForm(forms.ModelForm):
     first_name = forms.CharField(
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Ім’я"}),
+        label=_("Ім’я"),
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": _("Ім’я")}),
         required=True,
         error_messages={
-            'required': 'Будь ласка, введіть ваше ім’я.'
+            'required': _("Будь ласка, введіть ваше ім’я.")
         }
     )
     last_name = forms.CharField(
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Прізвище"}),
+        label=_("Прізвище"),
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": _("Прізвище")}),
         required=True,
         error_messages={
-            'required': 'Будь ласка, введіть ваше прізвище.'
+            'required': _("Будь ласка, введіть ваше прізвище.")
         }
     )
     phone = forms.CharField(
-        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Телефон"}),
+        label=_("Телефон"),
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": _("Телефон")}),
         required=True,
         error_messages={
-            'required': 'Будь ласка, введіть номер телефону.'
+            'required': _("Будь ласка, введіть номер телефону.")
         }
     )
 
     city = forms.CharField(
-        widget=forms.TextInput(attrs={"class": "form-control", "id": "city-input", "placeholder": "Місто"}),
+        label=_("Місто"),
+        widget=forms.TextInput(attrs={"class": "form-control", "id": "city-input", "placeholder": _("Місто")}),
         required=True,
         error_messages={
-            'required': 'Будь ласка, введіть назву міста.'
+            'required': _("Будь ласка, введіть назву міста.")
         }
     )
 
     warehouse = forms.CharField(
+        label=_("Відділення / Поштомат"),
         widget=forms.TextInput(
-            attrs={"class": "form-control", "id": "warehouse-input", "placeholder": "Відділення/Поштомат"}),
+            attrs={"class": "form-control", "id": "warehouse-input", "placeholder": _("Відділення/Поштомат")}
+        ),
         required=True,
         error_messages={
-            'required': 'Будь ласка, вкажіть відділення або поштомат.'
+            'required': _("Будь ласка, вкажіть відділення або поштомат.")
         }
     )
 
     def clean_phone(self):
         phone = self.cleaned_data["phone"]
-
         if not phone.isdigit():
-            raise forms.ValidationError("Номер телефону повинен містити лише цифри.")
+            raise forms.ValidationError(_("Номер телефону повинен містити лише цифри."))
         elif len(phone) < 10:
-            raise forms.ValidationError(
-                "Введіть дійсний номер телефону, який містить щонайменше 10 цифр."
-            )
+            raise forms.ValidationError(_("Введіть дійсний номер телефону, який містить щонайменше 10 цифр."))
         return phone
 
     class Meta:
