78198ce038ff9cb739849410433f635e1260c662
Danya Oleksuk <sinrandomg@gmail.com>
2025-11-06 20:48:48 +0200

    ref: add product urls file to access products endpoints

---
+++ api/v2/products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from api.v2.products import views
+
+
+app_name = "products"
+
+urlpatterns = [
+    path("", views.ProductListApi.as_view(), name="product-list"),
+    path("<int:pk>/", views.ProductDetailApi.as_view(), name="product-detail"),
+]
