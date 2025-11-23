940caa9b746106a2f9cae2651ceb609b125b668f
Danya Oleksuk <sinrandomg@gmail.com>
2025-11-23 19:01:27 +0200

    feat: add url to access ProductUpdateApi endpoint

--- api/v2/products/urls.py
+++ api/v2/products/urls.py
@@ -8,4 +8,5 @@ app_name = "products"
 urlpatterns = [
     path("", views.ProductListApi.as_view(), name="product-list"),
     path("<int:pk>/", views.ProductDetailApi.as_view(), name="product-detail"),
+    path("<int:pk>/update/", views.ProductUpdateApi.as_view(), name="product-update"),
 ]
