a2e1d2cc33494f0ed154059ccafd42e5a97af508
Danya Oleksuk <sinrandomg@gmail.com>
2025-12-05 23:29:02 +0200

    ref: add new products models imports

--- products/signals.py
+++ products/signals.py
@@ -2,7 +2,8 @@ from django.core.cache import cache
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from .models import Category, Product
+from products.models.product import Product
+from products.models.category import Category
 
 
 @receiver([post_save, post_delete], sender=Product)
