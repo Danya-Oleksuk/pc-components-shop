6872e0c97b7bd41b9a8eea2d92f062a619802ad9
Danya Oleksuk <sinrandomg@gmail.com>
2025-12-13 18:26:37 +0200

    fix: user view tests

--- users/tests.py
+++ users/tests.py
@@ -1,64 +1,64 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from orders.tests import User
 from users.forms import UserLoginForm, UserRegisterForm
 
 
 class UserModelTest(TestCase):
     def test_create_user_with_email(self):
         email = "testuser@example.com"
-        username = "testuser"
         password = "password123"
 
-        user = get_user_model().objects.create_user(
-            email=email, username=username, password=password
-        )
+        user = User.objects.create(email=email)
+        user.set_password(password)
+        user.save()
 
         self.assertEqual(user.email, email)
-        self.assertEqual(user.username, username)
         self.assertTrue(user.check_password(password))
         self.assertEqual(user.get_username(), email)
 
     def test_create_user_with_no_email(self):
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(
-                email="", username="testuser", password="password123"
-            )
+            User.objects.create(email="")
+
+        with self.assertRaises(ValueError):
+            User.objects.create(email=None)
 
     def test_create_superuser(self):
         email = "admin@example.com"
-        username = "admin"
         password = "adminpassword123"
 
-        admin_user = get_user_model().objects.create_superuser(
-            email=email, username=username, password=password
+        admin_user = User.objects.create(
+            email=email, password=password, is_staff=True, is_superuser=True
         )
 
         self.assertTrue(admin_user.is_superuser)
         self.assertTrue(admin_user.is_staff)
         self.assertEqual(admin_user.email, email)
-        self.assertEqual(admin_user.username, username)
 
     def test_user_email_is_unique(self):
         email = "testuser@example.com"
-        username = "testuser"
         password = "password123"
 
-        get_user_model().objects.create_user(
-            email=email, username=username, password=password
+        User.objects.create(
+            email=email,
+            password=password,
         )
 
         with self.assertRaises(Exception):
-            get_user_model().objects.create_user(
-                email=email, username="newuser", password="newpassword123"
+            User.objects.create(
+                email=email,
+                password="newpassword123",
             )
 
 
 class UserLoginFormTest(TestCase):
     def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            email="testuser@example.com", password="testpassword", username="testuser"
+        self.user = User.objects.create(
+            email="testuser@example.com",
         )
+        self.user.set_password("testpassword")
+        self.user.save()
 
     def test_login_form_valid(self):
         form_data = {"username": "testuser@example.com", "password": "testpassword"}
@@ -73,6 +73,7 @@ class UserLoginFormTest(TestCase):
     def test_login_user_authenticated(self):
         form_data = {"username": "testuser@example.com", "password": "testpassword"}
         form = UserLoginForm(data=form_data)
+
         self.assertTrue(form.is_valid())
         user = form.get_user()
         self.assertEqual(user.email, self.user.email)
@@ -103,6 +104,7 @@ class UserRegisterFormTest(TestCase):
         form = UserRegisterForm(data=self.user_data)
         self.assertTrue(form.is_valid())
         user = form.save()
+
         self.assertEqual(user.email, self.user_data["email"])
         self.assertTrue(user.check_password(self.user_data["password1"]))
 
