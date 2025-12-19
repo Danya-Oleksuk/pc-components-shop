8536caeab9d25e4cf86f29d0316af7b6d31bff17
Danya Oleksuk <sinrandomg@gmail.com>
2025-12-19 09:58:20 +0200

    ref: add OrderDetailApi

--- api/v2/orders/views.py
+++ api/v2/orders/views.py
@@ -1,6 +1,8 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from api.v2.orders.serializers import (
     OrderDisplaySerializer,
 )
@@ -12,3 +14,14 @@ class OrderListApi(ListAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Order.objects.all()
     search_fields = ("user__username", "status", "first_name", "last_name")
+
+
+class OrderDetailApi(GenericAPIView):
+    serializer_class = OrderDisplaySerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Order.objects.all()
+
+    def get(self, request, pk: int):
+        order = self.get_object()
+        serializer = self.serializer_class(order)
+        return Response(serializer.data, status=status.HTTP_200_OK)
