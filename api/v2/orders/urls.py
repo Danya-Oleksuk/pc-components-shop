bf679897293ae8e0a13601a27f708a0f6e6a6ac9
Danya Oleksuk <sinrandomg@gmail.com>
2025-12-20 18:55:49 +0200

    ref: add 2 endpoints to access OrderUpdateApi and OrderDeleteApi

--- api/v2/orders/urls.py
+++ api/v2/orders/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("", views.OrderListApi.as_view(), name="order-list"),
     path("<int:pk>/", views.OrderDetailApi.as_view(), name="order-detail"),
     path("create/", views.OrderCreateApi.as_view(), name="order-create"),
+    path("<int:pk>/update/", views.OrderUpdateApi.as_view(), name="order-update"),
+    path("<int:pk>/delete/", views.OrderDeleteApi.as_view(), name="order-delete"),
 ]
