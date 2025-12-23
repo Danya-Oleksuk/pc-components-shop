7c9d78fb66e04c56b848fe95a134338c29a6109b
Danya Oleksuk <sinrandomg@gmail.com>
2025-12-23 09:51:54 +0200

    ref: add endpoint for AuthTokenObtainView

--- api/v2/users/urls.py
+++ api/v2/users/urls.py
@@ -7,4 +7,7 @@ app_name = "users"
 
 urlpatterns = [
     path("", views.UserListApi.as_view(), name="user-list"),
+    path(
+        "token/obtain/", views.AuthTokenObtainView.as_view(), name="user-token-obtain"
+    ),
 ]
