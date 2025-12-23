5de9d400d8102043ccb423aded62cb5f938295a6
Danya Oleksuk <sinrandomg@gmail.com>
2025-12-23 09:51:20 +0200

    ref: add TokenObtainSerializer

--- api/v2/users/serializers.py
+++ api/v2/users/serializers.py
@@ -3,6 +3,8 @@ from api.mixins import (
     ReadOnlySerializerMixin,
 )
 from users.models.users import User
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import authenticate
 
 
 class UserDisplaySerializer(ReadOnlySerializerMixin, serializers.ModelSerializer):
@@ -15,3 +17,25 @@ class UserDisplaySerializer(ReadOnlySerializerMixin, serializers.ModelSerializer
             "last_name",
             "created_at",
         )
+
+
+class TokenObtainSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(style={"input_type": "password"})
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            request=self.context.get("request"),
+            email=email,
+            password=password,
+        )
+
+        if not user:
+            msg = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError({"message": msg}, code="authorization")
+
+        attrs["user"] = user
+        return attrs
