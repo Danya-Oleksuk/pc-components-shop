261c102e356a38af6272a52dcdcbe6d8fb19e560
Danya Oleksuk <sinrandomg@gmail.com>
2025-12-23 10:01:23 +0200

    ref: add 3 endpoints to reset user password

--- api/v2/users/urls.py
+++ api/v2/users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from django_rest_passwordreset.views import (
+    reset_password_confirm,
+    reset_password_request_token,
+    reset_password_validate_token,
+)
+
 from api.v2.users import views
 
 
@@ -9,5 +15,20 @@ urlpatterns = [
     path("", views.UserListApi.as_view(), name="user-list"),
     path(
         "token/obtain/", views.AuthTokenObtainView.as_view(), name="user-token-obtain"
+    ),
+    path(
+        "password/reset/",
+        reset_password_request_token,
+        name="user-password-reset",
+    ),
+    path(
+        "password/reset/confirm/",
+        reset_password_confirm,
+        name="user-password-reset-confirm",
+    ),
+    path(
+        "password/reset/validate/",
+        reset_password_validate_token,
+        name="user-password-reset-validate",
     ),
 ]
