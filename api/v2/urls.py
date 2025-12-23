862a8f04dbb9e613403bdd621ad93d230f91f116
Danya Oleksuk <sinrandomg@gmail.com>
2025-12-23 09:47:01 +0200

    feat: add users urls v2 api

--- api/v2/urls.py
+++ api/v2/urls.py
@@ -5,4 +5,5 @@ app_name = "v2"
 urlpatterns = [
     path("products/", include("api.v2.products.urls")),
     path("orders/", include("api.v2.orders.urls")),
+    path("users/", include("api.v2.users.urls")),
 ]
