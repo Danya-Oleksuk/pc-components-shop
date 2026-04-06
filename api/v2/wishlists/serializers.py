fdf7cc028a86302fc857927b9abd3b3dd04f3017
Danya Oleksuk <sinrandomg@gmail.com>
2026-04-06 18:28:55 +0300

    feat(serializers): add WishlistCreateSerializer, WishlistUpdateSerializer

--- api/v2/wishlists/serializers.py
+++ api/v2/wishlists/serializers.py
@@ -1,9 +1,8 @@
 from rest_framework import serializers
-from api.mixins import (
-    ReadOnlySerializerMixin,
-)
+from api.mixins import ReadOnlySerializerMixin, UpdateOnlySerializerMixin
+from products.services.crud import wishlist_create, wishlist_update
 from api.v2.products.serializers import ProductDisplaySerializer
-from products.models import Wishlist
+from products.models import Product, Wishlist
 
 
 class WishlistDisplaySerializer(ReadOnlySerializerMixin, serializers.ModelSerializer):
@@ -28,3 +27,27 @@ class WishlistDisplaySerializer(ReadOnlySerializerMixin, serializers.ModelSerial
         if not is_admin:
             fields.pop("user", None)
         return fields
+
+
+class WishlistCreateSerializer(serializers.Serializer):
+    product = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        required=True,
+    )
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        assert request is not None, "Request is required"
+        assert not request.user.is_anonymous, "Authentication is required"
+        user = request.user
+        return wishlist_create(user=user, **validated_data)
+
+
+class WishlistUpdateSerializer(UpdateOnlySerializerMixin, serializers.Serializer):
+    product = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        required=True,
+    )
+
+    def update(self, instance, validated_data):
+        return wishlist_update(wishlist=instance, **validated_data)
