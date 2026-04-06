4b5bd4bd39cc37d3ea573ccff9ac6b7189b2c0ec
Danya Oleksuk <sinrandomg@gmail.com>
2026-04-06 18:00:02 +0300

    feat(urls): add WishlistDeleteView endpoint import

--- api/v2/wishlists/urls.py
+++ api/v2/wishlists/urls.py
@@ -6,4 +6,7 @@ app_name = "wishlists"
 
 urlpatterns = [
     path("", views.WishlistListView.as_view(), name="wishlist-list"),
+    path(
+        "<int:pk>/delete/", views.WishlistDeleteView.as_view(), name="wishlist-delete"
+    ),
 ]
