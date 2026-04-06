5d9c7cf1579f3b73c58e13dd7c15bea5f980c707
Danya Oleksuk <sinrandomg@gmail.com>
2026-04-06 18:29:55 +0300

    feat: add wishlist update and delete endpoints

--- api/v2/wishlists/urls.py
+++ api/v2/wishlists/urls.py
@@ -6,6 +6,10 @@ app_name = "wishlists"
 
 urlpatterns = [
     path("", views.WishlistListView.as_view(), name="wishlist-list"),
+    path("create/", views.WishlistCreateView.as_view(), name="wishlist-create"),
+    path(
+        "<int:pk>/update/", views.WishlistUpdateView.as_view(), name="wishlist-update"
+    ),
     path(
         "<int:pk>/delete/", views.WishlistDeleteView.as_view(), name="wishlist-delete"
     ),
